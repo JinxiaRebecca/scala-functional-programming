56e72d91bbc9c81d7976f3ef972a780c2f55a059
JinxiaRebecca <18502908857@163.com>
2022-08-20 17:25:12 +0800

    option encode

--- src/main/scala/ByteEncoder.scala
+++ src/main/scala/ByteEncoder.scala
@@ -6,16 +6,20 @@ trait ByteEncoder[A] {
 object ByteEncoder {
   def apply[A](implicit ev: ByteEncoder[A]): ByteEncoder[A] = ev
 
-  implicit object StringByteEncoder extends ByteEncoder[String] {
-    override def encode(a: String): Array[Byte] = a.getBytes
+}
+implicit object StringByteEncoder extends ByteEncoder[String] {
+  override def encode(a: String): Array[Byte] = a.getBytes
+}
+
+implicit def optionEncoder[A](implicit encA: ByteEncoder[A]): ByteEncoder[Option[A]] = new ByteEncoder[Option[A]] {
+  override def encode(a: Option[A]): Array[Byte] = {
+    a match {
+      case Some(value) => encA.encode(value)
+      case None => Array[Byte]()
+    }
   }
-
 }
 
-implicit object Rot3StringByteEncoder extends ByteEncoder[String] {
-  override def encode(a: String): Array[Byte] =
-    a.getBytes.map(b => (b + 3).toByte)
-}
 
 ByteEncoder[String].encode("hello")
 
